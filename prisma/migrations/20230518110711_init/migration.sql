737db348e66b5fc939f9016fe5fbab5f8ba9fd22
Seraphic <danilsocol-2002@mail.ru>
2023-05-18 16:07:58 +0500

    prisma mirgrate init

---
+++ prisma/migrations/20230518110711_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "email" TEXT NOT NULL,
+    "user_id" SERIAL NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_user_id_key" ON "User"("user_id");
