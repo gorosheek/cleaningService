2f1729430fab28e043efd9f4bc1339519e6eff08
groshenk2 <groshenk2@intersvyaz.net>
2023-06-22 21:53:16 +0500

    remove old routes and add new route

--- src/routes/order-router.js
+++ src/routes/order-router.js
@@ -25,61 +25,13 @@ const router = Router()
  *             schema:
  *               $ref: '#/components/schemas/Order'
  */
-router.post('/order', OrderController.create)
-/**
- * @openapi
- * '/orderGateway':
- *  post:
- *     tags:
- *     - Order
- *     requestBody:
- *      required: true
- *      content:
- *        application/json:
- *           schema:
- *              $ref: '#/components/schemas/ReqOrderGetAway'
- *     responses:
- *      200:
- *        description: Success
- */
-router.post('/orderGateway', OrderController.createGatewayOrder)
-/**
- * @openapi
- * '/changeStatusToBooked':
- *  post:
- *     tags:
- *     - Order
- *     summary: Change status of order for booked
- *     requestBody:
- *      required: true
- *      content:
- *        application/json:
- *           schema:
- *              $ref: '#/components/schemas/ReqChangeStatusOrder'
- *     responses:
- *      200:
- *        description: Success
- *        content:
- *          application/json:
- *             schema:
- *               $ref: '#/components/schemas/Order'
- */
 
-router.post('/orderHotelService', OrderController.createHotelServiceOrder)
-router.post('/changeStatusToBooked', OrderController.changeStatusToBooked)
 /**
  * @openapi
- * '/changeStatusToCleanUp':
- *  post:
+ * '/orders':
+ *  get:
  *     tags:
  *     - Order
- *     summary: Change status of order for clean up
- *     requestBody:
- *      required: true
- *      content:
- *        application/json:
- *           schema:
- *              $ref: '#/components/schemas/ReqChangeStatusOrder'
  *     responses:
  *      200:
  *        description: Success
@@ -88,28 +40,6 @@ router.post('/changeStatusToBooked', OrderController.changeStatusToBooked)
  *             schema:
  *               $ref: '#/components/schemas/Order'
  */
-router.post('/changeStatusToCleanUp', OrderController.changeStatusToCleanUp)
-/**
- * @openapi
- * '/changeStatusToCompleted':
- *  post:
- *     tags:
- *     - Order
- *     summary: Change status of order for competed
- *     requestBody:
- *      required: true
- *      content:
- *        application/json:
- *           schema:
- *              $ref: '#/components/schemas/ReqChangeStatusOrder'
- *     responses:
- *      200:
- *        description: Success
- *        content:
- *          application/json:
- *             schema:
- *               $ref: '#/components/schemas/Order'
- */
-router.post('/changeStatusToCompleted', OrderController.changeStatusToCompleted)
+router.get('/orders', OrderController.getAllOrders)
 
 export default router
